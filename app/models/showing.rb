e29b31e861d325ffc8eeeaa22808755a4f449e61
Sarah Mustafa <sarahmustafa@dyn-86-36-43-60.wv.qatar.cmu.edu>
2012-12-04 10:53:42 +0300

    with showing

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -1,6 +1,6 @@
 class Showing < ActiveRecord::Base
   attr_accessible :movie_id, :number_of_available_seats, :showing_time
-  belongs_to :movie
+  #belongs_to :movie
   validate :number_of_available_seats, :showing_time
   
 
