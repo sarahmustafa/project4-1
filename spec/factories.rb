3915b0cc563398fcd8909aeceb7d9c3f61aeb385
Haya Thowfeek <hthowfee@qatar.cmu.edu>
2012-12-09 10:40:54 +0300

    Pulling sarah's master

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,12 @@ FactoryGirl.define do
     email "zuhair.ghalib@gmail.com"
     number_of_seats 5
   end
+
+  factory :movie do
+  	cast "Brad Pitt, Edward Norton, Jared Leto"
+  	duration "139 mins"
+  	genre "Thriller"
+  	rating "R"
+  	title "Fight Club"
+  end 
 end
